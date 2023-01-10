d310e0050775836f9c2d50ac71cd754a95c540c8
Flashy990 <108693136+Flashy990@users.noreply.github.com>
2023-01-09 20:38:20 -0500

    Add files via upload

---
+++ Chess/game_engine.py
@@ -0,0 +1,17 @@
+# This class stores all the information about the current state of the game.
+# It will also check if the moves are valid
+# It will also store a move log
+class game_state():
+    def __init__(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.white_move = True
+        self.move_log = []
